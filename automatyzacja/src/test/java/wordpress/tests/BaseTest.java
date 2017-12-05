62c10989ac1eb166bba896d2576a811aae308992
rafal Markowicz <rafal.markowicz@codesprinters.com>
2017-12-05 23:11:24 +0100

    renamed driver timeout constants to make clear these are set in seconds

--- automatyzacja/src/test/java/wordpress/tests/BaseTest.java
+++ automatyzacja/src/test/java/wordpress/tests/BaseTest.java
@@ -4,7 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -13,16 +12,16 @@ import java.util.concurrent.TimeUnit;
  * Created by Rafal on 2017-12-05.
  */
 public class BaseTest {
-    private static final int DRIVER_IMPLICIT_WAIT_TIMEOUT = 15;
-    private static final int DRIVER_PAGE_LOAD_TIMEOUT = 15;
+    private static final int DRIVER_IMPLICIT_WAIT_TIMEOUT_IN_SECONDS = 15;
+    private static final int DRIVER_PAGE_LOAD_TIMEOUT_IN_SECONDS = 15;
     protected WebDriver driver;
 
     @Before
     public void initWebDriver() throws IOException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(DRIVER_IMPLICIT_WAIT_TIMEOUT, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(DRIVER_PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(DRIVER_IMPLICIT_WAIT_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(DRIVER_PAGE_LOAD_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
     }
 
     @After
